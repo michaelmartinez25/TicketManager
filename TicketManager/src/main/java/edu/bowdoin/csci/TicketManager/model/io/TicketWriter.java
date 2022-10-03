050e09cbb6157ed4a4b94bd8562ff702967555b0
Michael Martinez <mmartinez@bowdoin.edu>
2022-10-03 11:33:00 -0400

    [Stub] Stub out TicketWriter Class

---
+++ TicketManager/src/main/java/edu/bowdoin/csci/TicketManager/model/io/TicketWriter.java
@@ -0,0 +1,34 @@
+package edu.bowdoin.csci.TicketManager.model.io;
+
+import java.util.List;
+
+import edu.bowdoin.csci.TicketManager.model.ticket.Ticket;
+
+/**
+ * Writes ticket files
+ * 
+ * @author mmartinez
+ */
+public class TicketWriter {
+	
+	/**
+	 * Constructor method
+	 */
+	public TicketWriter() {
+		
+	}
+	
+	/**
+	 * Receives a String with the file name to write to and a List of Tickets
+	 * to write. Uses Ticket's toString() method to create the properly formatted
+	 * output for a Ticket. If there are any errors, an IllegalArgumentException
+	 * is thrown.
+	 * 
+	 * @param filename file to write to 
+	 * @param ticketList list of Tickets to write
+	 * @throws IllegalArgumentException if there are any errors
+	 */
+	public static void writeTicketFile(String filename, List<Ticket> ticketList) {
+		
+	}
+}
