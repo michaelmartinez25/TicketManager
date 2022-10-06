6d3b3794472b2c9ab184468b0a55becd48c53b1f
Zeb Becker <zbecker2@bowdoin.edu>
2022-10-06 14:46:49 -0400

    [Test] Adds test folder

---
+++ TicketManager/src/test/java/edu/bowdoin/csci/TicketManager/model/command/commandTest.java
@@ -0,0 +1,11 @@
+package edu.bowdoin.csci.TicketManager.model.command;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class commandTest {
+	
+	public void testTest() {
+		assertTrue(true); 
+	}
+}
