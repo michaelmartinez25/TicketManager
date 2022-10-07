3b2f07cff557302417ba9888e7bc196312ea3817
Michael Martinez <mmartinez@bowdoin.edu>
2022-10-07 11:31:09 -0400

    [Test] Add Test Stubs for Manager Classes

---
+++ TicketManager/src/test/java/edu/bowdoin/csci/TicketManager/model/manager/TicketListTest.java
@@ -0,0 +1,45 @@
+package edu.bowdoin.csci.TicketManager.model.manager;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TicketListTest {
+
+	private TicketList list;
+	
+	/**
+	 * Sets up the test.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	public void setUp() throws Exception {
+		list = new TicketList();
+	}
+	
+	@Test
+	public void testTicketList() {
+		
+	}
+	
+	@Test
+	public void testAddTicket() {
+		
+	}
+	
+	@Test
+	public void testDeleteTicket() {
+		
+	}
+	
+	@Test
+	public void testGetTicket() {
+		
+	}
+	
+	@Test
+	public void testExecuteCommand() {
+		
+	}
+}
---
+++ TicketManager/src/test/java/edu/bowdoin/csci/TicketManager/model/manager/TicketManagerTest.java
@@ -0,0 +1,65 @@
+package edu.bowdoin.csci.TicketManager.model.manager;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class TicketManagerTest {
+	
+	private TicketManager manager;
+	
+	/**
+	 * Sets up the test.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	public void setUp() throws Exception {
+		manager = TicketManager.getInstance();
+	}
+	
+	@Test
+	public void testTicketManager() {
+		
+	}
+	
+	@Test
+	public void testSavingTickets() {
+		
+	}
+	
+	@Test
+	public void testLoadingTickets() {
+		
+	}
+	
+	@Test
+	public void testNewTicketList() {
+		
+	}
+	
+	public void testDisplayTickets() {
+		
+	}
+	
+	@Test
+	public void testAddTicket() {
+		
+	}
+	
+	@Test
+	public void testRemoveTicket() {
+		
+	}
+	
+	@Test
+	public void testGetTicket() {
+		
+	}
+	
+	@Test
+	public void testExecuteCommand() {
+		
+	}
+}
