5d673ba3d2ece10c88478096b30a05246aac2f8a
Zeb Becker <zbecker2@bowdoin.edu>
2022-10-19 11:51:37 -0400

    [Test] Adds initial tests for TicketWriter.

--- TicketManager/src/main/java/edu/bowdoin/csci/TicketManager/model/io/TicketWriter.java
+++ TicketManager/src/main/java/edu/bowdoin/csci/TicketManager/model/io/TicketWriter.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import edu.bowdoin.csci.TicketManager.model.ticket.Ticket;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.io.FileWriter;
 /**
  * Writes ticket files
  * 
@@ -28,5 +31,57 @@ public class TicketWriter {
 	 */
 	public static void writeTicketFile(String filename, List<Ticket> ticketList) {
 		//comment for PMD
+		IllegalArgumentException iae = new IllegalArgumentException("Unable to save file."); 
+		
+		if (filename == null || ticketList == null) {
+			throw iae; 
+		}
+		
+		String output = ""; 
+		
+		for (Ticket t: ticketList) {
+			output += TicketWriter.writeTicket(t);
+		}
+		
+		try {
+			//append = false. Overwrite file. 
+			FileWriter writer = new FileWriter(filename, false); 
+			writer.write(output); 
+			writer.close(); 
+		}
+		catch (Exception e) {
+			throw iae; 
+		}
+		
+		
+	}
+	
+	/**
+	 * Helper method to create string representation of ticket with correct delimiters for storage
+	 * @param t ticket 
+	 * @return string representing ticket to write to file
+	 */
+	private static String writeTicket(Ticket t) {
+		
+		String output = "*"; 
+		
+		output += t.getTicketId();
+		output += "#" + t.getState(); 
+		output += "#" + t.getTicketTypeString(); 
+		output += "#" + t.getSubject(); 
+		output += "#" + t.getCaller(); 
+		output += "#" + t.getCategory();
+		output += "#" + t.getPriority(); 
+		output += "#" + t.getOwner(); 
+		//assumes that unused codes are stored as null
+		output += "#"; 
+		if (t.getCancellationCode() != null) output += t.getCancellationCode();
+		if (t.getFeedbackCode() != null) output += t.getFeedbackCode(); 
+		if (t.getResolutionCode() != null) output += t.getResolutionCode(); 
+		output += "\n"; 
+		
+		output += t.getNotes(); 
+		
+		return output; 
 	}
 }
--- TicketManager/src/test/java/edu/bowdoin/csci/TicketManager/model/io/IoTest.java
+++ TicketManager/src/test/java/edu/bowdoin/csci/TicketManager/model/io/IoTest.java
@@ -36,133 +36,133 @@ public class IoTest {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket3.txt");
+			TicketReader.readTicketFile("test-files/ticket3.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket4.txt");
+			TicketReader.readTicketFile("test-files/ticket4.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket5.txt");
+			TicketReader.readTicketFile("test-files/ticket5.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket6.txt");
+			TicketReader.readTicketFile("test-files/ticket6.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket7.txt");
+			TicketReader.readTicketFile("test-files/ticket7.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket8.txt");
+			TicketReader.readTicketFile("test-files/ticket8.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket9.txt");
+			TicketReader.readTicketFile("test-files/ticket9.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket10.txt");
+			TicketReader.readTicketFile("test-files/ticket10.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket11.txt");
+			TicketReader.readTicketFile("test-files/ticket11.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket12.txt");
+			TicketReader.readTicketFile("test-files/ticket12.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket13.txt");
+			TicketReader.readTicketFile("test-files/ticket13.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket14.txt");
+			TicketReader.readTicketFile("test-files/ticket14.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket15.txt");
+			TicketReader.readTicketFile("test-files/ticket15.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket16.txt");
+			TicketReader.readTicketFile("test-files/ticket16.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket17.txt");
+			TicketReader.readTicketFile("test-files/ticket17.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket18.txt");
+			TicketReader.readTicketFile("test-files/ticket18.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket19.txt");
+			TicketReader.readTicketFile("test-files/ticket19.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("ticket20.txt");
+			TicketReader.readTicketFile("test-files/ticket20.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
 			//Exception expected: carry on
 		}
 		try {
-			TicketReader.readTicketFile("does-not-exist.txt");
+			TicketReader.readTicketFile("test-files/does-not-exist.txt");
 			fail("Should throw IAE on invalid file."); 
 		}
 		catch (IllegalArgumentException iae) {
@@ -185,14 +185,59 @@ public class IoTest {
 		
 	}
 	
-	
 	/**
-	 * Test ticketWriter
+	 * test writer with valid files
 	 */
 	@Test
-	public void fakeWriterTest() {
-		TicketWriter.writeTicketFile("filename", null);
-		assertEquals(1, 1); 
+	public void testWriterValidFile() {
+		
+		List<Ticket> tickets = TicketReader.readTicketFile("test-files/ticket1.txt"); 
+		TicketWriter.writeTicketFile("test-files/testfile.txt", tickets); 
+		List<Ticket> fromFile = TicketReader.readTicketFile("test-files/testfile.txt"); 
+		assertEquals(tickets.size(), fromFile.size()); 
+		
+		tickets = TicketReader.readTicketFile("test-files/ticket2.txt"); 
+		TicketWriter.writeTicketFile("test-files/testfile.txt", tickets); 
+		fromFile = TicketReader.readTicketFile("test-files/testfile.txt"); 
+		assertEquals(tickets.size(), fromFile.size()); 
+		
+		
+		
+	}
+	
+	/**
+	 * Test ticketWriter on invalid files
+	 */
+	@Test
+	public void testWriterInvalidFiles() {
+		try {
+			TicketWriter.writeTicketFile("test-files/testfile.txt", null);
+			fail("Null ticket List should throw IAE"); 
+		}
+		catch (IllegalArgumentException iae) {
+			//exception expected; carry on. 
+		}
+		try {
+			List<Ticket> tickets = TicketReader.readTicketFile("test-files/ticket1.txt"); 
+			TicketWriter.writeTicketFile(null, tickets); 
+		}
+		catch (IllegalArgumentException iae) {
+			//exception expected; carry on. 
+		}
+		
+		/** 
+		 * Not actually sure if it is not allowed to write to new file
+		 * 
+		try {
+			List<Ticket> tickets = TicketReader.readTicketFile("test-files/ticket1.txt"); 
+			TicketWriter.writeTicketFile("test-files/this_file_does_not_exist.txt", tickets); 
+			fail("Non-existent output file should throw IAE"); 
+		}
+		catch (IllegalArgumentException iae) {
+			//exception expected; carry on.
+		}
+		*/
+		
 	}
 	
 	
